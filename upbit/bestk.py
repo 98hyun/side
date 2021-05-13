87a73c5e45a5c0fcbad7c591112f697fe7823360
98hyun <98hyun@kakao.com>
2021-05-13 09:48:02 +0900

    20210513 pyupbit

---
+++ upbit/bestk.py
@@ -0,0 +1,20 @@
+import pyupbit
+import numpy as np
+
+
+def get_ror(k=0.5):
+    df = pyupbit.get_ohlcv("KRW-ETH")
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+
+for k in np.arange(0.001, 1.0,0.05):
+    ror = get_ror(k)
+    print("%.4f %f" % (k, ror))
